4bab6a84d1f00c8404eb30acc3bb838c2d57a6c6
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-10-22 21:48:01 +0300

    first shadcn component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>Hello admin dashboard</p>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <div className="p-4">
+        <Button size="default" variant="destructive">
+          Click Me
+        </Button>
+      </div>
+    </div>
+  );
 }
