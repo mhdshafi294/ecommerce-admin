3a5f4f01dce4c8aa0d5e61132e10dcfd847f6461
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-10-25 23:27:22 +0300

    initial test for the modal

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,15 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <div className="p-4">
-        <Button size="default" variant="destructive">
-          Click Me
-        </Button>
-      </div>
+    <div className="p-4">
+      <Modal title="test" description="Test Desc" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
 }
