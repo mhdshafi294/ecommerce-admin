4b4c96a09c44e447ea96f389c4eda3f4b479a033
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-11-08 23:21:23 +0300

    start working on the store settings

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,9 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
+
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
@@ -16,7 +19,17 @@ const Navbar = async () => {
     },
   });
 
-  return <div className="border-b"></div>;
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
