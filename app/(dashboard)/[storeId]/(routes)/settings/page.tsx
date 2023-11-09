f420b7e52c78d14f76aa270583b789dd16645061
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-11-09 19:42:44 +0300

    alert modal component is done

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
+import { SettingsForm } from "./components/settings-form";
 
 // import { SettingsForm } from "./components/settings-form";
 
@@ -26,7 +27,7 @@ const SettingsPage = async ({ params }: { params: { storeId: string } }) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        {/* <SettingsForm initialData={store} /> */}
+        <SettingsForm initialData={store} />
       </div>
     </div>
   );
