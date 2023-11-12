62183ff9dea5428fd6e86938827b1cccbb431906
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-11-12 21:19:03 +0300

    cell actions & api-list for billboard

--- app/(dashboard)/[storeId]/(routes)/billboard/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboard/components/client.tsx
@@ -32,7 +32,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} />
+      <DataTable searchKey="label" columns={columns} data={data} />
       <Heading title="API" description="API Calls for Billboards" />
       <Separator />
     </>
