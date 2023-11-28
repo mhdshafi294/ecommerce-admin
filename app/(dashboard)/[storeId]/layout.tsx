24c359d665ebb7c3c4d1fe4daa3aa16e45b30f70
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-11-28 04:21:23 +0300

    dark mode

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,9 +1,8 @@
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 
-// import Navbar from "@/components/navbar";
-import prismadb from "@/lib/prismadb";
 import Navbar from "@/components/navbar";
+import prismadb from "@/lib/prismadb";
 
 export default async function DashboardLayout({
   children,
